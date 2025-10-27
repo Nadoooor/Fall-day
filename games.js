04c6fb53786d5ab1bb42de9b7d694a2af54d5011
Nader Sayed <nadersayed742@gmail.com>
2025-10-27 06:10:16 +0300

    finished

--- games.js
+++ games.js
@@ -42,3 +42,49 @@ AFRAME.registerComponent('hidden', {
         });
     }
 });
+
+
+AFRAME.registerComponent('ups', {
+    init: function ()   {
+    {
+        this.hornet = document.querySelector('[gltf-model="#Hornet"]');
+        this.pat = document.querySelector('[gltf-model="#pat"]');
+        this.balls = document.querySelectorAll('[gltf-model="#ball"]');
+        this.trees = document.querySelectorAll('.tree');
+        this.el.addEventListener('click', () => {
+            console.log('Upgrade clicked');
+            this.hornet.setAttribute('scale', '3 3 3');
+            this.pat.setAttribute('scale', '3 3 3');
+            this.balls.forEach((ball) => {
+                ball.setAttribute('scale', '0.7 0.7 0.7');
+            });
+            this.trees.forEach((tree) => {
+                tree.setAttribute('scale', '2 2 2');
+            });
+        });
+    }}});
+
+    AFRAME.registerComponent('uus', {
+    init: function ()   {
+    {
+        this.hornet = document.querySelector('[gltf-model="#Hornet"]');
+        this.pat = document.querySelector('[gltf-model="#pat"]');
+        this.balls = document.querySelectorAll('[gltf-model="#ball"]');
+        this.trees = document.querySelectorAll('.tree');
+        this.el.addEventListener('click', () => {
+            console.log('Upgrade clicked');
+            this.hornet.setAttribute('rotation', '180 0 0');
+            this.hornet.setAttribute('position', '0 13 0');
+            this.pat.setAttribute('rotation', '180 0 0');
+            this.balls.forEach((ball) => {
+                ball.setAttribute('rotation', '180 0 0');
+            });
+            this.trees.forEach((tree) => {
+                tree.setAttribute('rotation', '180 0 0');
+            });
+        });
+    }}});
+
+
+
+
